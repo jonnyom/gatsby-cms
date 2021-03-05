d8be0c94be2cb5b14c62af02bc31843dd4451faf
Jonny <jonathanomahony@gmail.com>
2021-03-05 19:13:02 +0000

    Update a few small bits and pieces

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,35 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from 'react';
+import { Link } from 'gatsby';
+import logo from '../img/logo2.svg';
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: ''
+    };
   }
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
       {
-        active: !this.state.active,
+        active: !this.state.active
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: 'is-active'
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: ''
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
@@ -76,23 +75,11 @@ const Navbar = class extends React.Component {
                 Form Examples
               </Link>
             </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/index.md
+++ src/pages/index.md
@@ -1,8 +1,8 @@
 ---
 templateKey: index-page
-title: Great coffee with a conscience
+title: Fantastic coaching, fantastic coffee
 image: /img/home-jumbotron.jpg
-heading: Great coffee with a conscience
+heading: Great coaching, great coffee (lol)
 subheading: Support sustainable farming while enjoying a cup
 mainpitch:
   title: Why Kaldi
