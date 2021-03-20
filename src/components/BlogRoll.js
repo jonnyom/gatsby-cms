b35193bd3151e6d0df82a5591ad6aab89dd08f14
Jonny <jonathanomahony@gmail.com>
2021-03-20 20:28:08 +0000

    Redesign blog roll

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -3,60 +3,62 @@ import PropTypes from 'prop-types';
 import { Link, graphql, StaticQuery } from 'gatsby';
 import PreviewCompatibleImage from './PreviewCompatibleImage';
 
-class BlogRoll extends React.Component {
-  render() {
-    const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
+const BlogPreview = ({ post }) => {
+  console.log(post);
+  return (
+    <div class="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
+      <a href="#" class="w-full block h-full">
+        {post.frontmatter.featuredimage ? (
+          // <div className="max-h-40 w-full object-cover">
+          <PreviewCompatibleImage
+            imageInfo={{
+              image: post.frontmatter.featuredimage,
+              alt: `featured image thumbnail for post ${post.frontmatter.title}`
+            }}
+          />
+        ) : // </div>
+        null}
+        <div class="bg-white dark:bg-gray-800 w-full p-4">
+          <Link
+            className="title has-text-primary is-size-4"
+            to={post.fields.slug}
+          >
+            {post.frontmatter.title}
+          </Link>
+          <span> &bull; </span>
+          <span className="subtitle is-size-5 is-block">
+            {post.frontmatter.date}
+          </span>
+          <p>
+            {post.excerpt}
+            <br />
+            <br />
+            <Link className="button" to={post.fields.slug}>
+              Keep Reading →
+            </Link>
+          </p>
+        </div>
+      </a>
+    </div>
+  );
+};
 
-    return (
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
-          ))}
+const BlogRoll = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+
+  return (
+    <div class="w-full bg-white p-12">
+      <div class="header flex items-end justify-between mb-12">
+        <div class="title">
+          <p class="text-4xl font-bold text-gray-800 mb-4">Lastest articles</p>
+        </div>
       </div>
-    );
-  }
-}
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12">
+        {posts && posts.map(({ node: post }) => <BlogPreview post={post} />)}
+      </div>
+    </div>
+  );
+};
 
 BlogRoll.propTypes = {
   data: PropTypes.shape({
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as CallToActionIntro } from './CallToActionIntro.js';
 export { default as NavBar } from './NavBar.js';
 export { default as Footer } from './Footer.js';
+export { default as BlogRoll } from './BlogRoll.js';
