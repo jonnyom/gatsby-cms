32accd7e29bb466b67ad942fe7662bd342f64425
Jonny <jonathanomahony@gmail.com>
2021-03-21 18:13:15 +0000

    Try this?

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as CallToActionIntro } from './CallToActionIntro.js';
-export { default as NavBar } from './NavBar.js';
-export { default as Footer } from './Footer.js';
-export { default as BlogRoll } from './BlogRoll.js';
+export { default as CallToActionIntro } from './CallToActionIntro';
+export { default as NavBar } from './NavBar';
+export { default as Footer } from './Footer';
+export { default as BlogRoll } from './BlogRoll';
 export { default as Layout } from './Layout';
 export { default as BlogPreview } from './BlogPreview';
